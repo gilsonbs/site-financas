882f8d2bc11e48549b994b374d3c95dd3dc7ec18
GilsonBarreto <gilson.barreto2009@gmail.com>
2024-12-10 08:17:48 -0300

    atualização de layout, estilisando o css do site, adicionando o hero ao layout

--- app.py
+++ app.py
@@ -6,5 +6,21 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+@app.route('/calculadoras')
+def calculadoras():
+    return render_template('calculadoras.html')
+
+@app.route('/sobre')
+def sobre():
+    return render_template('sobre.html')
+
+@app.route('/contato')
+def contato():
+    return render_template('contato.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
